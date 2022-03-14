22632d1a1bc25c2e66bbb22c273ac77622df91ae
Rosemary Gellene <gellener@bc.edu>
2022-03-13 20:55:05 -0400

    6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Rosemary Gellene on 3/3/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
