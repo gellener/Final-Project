20ff396e980d9473a73075dccb0996eb20821326
Rosemary Gellene <gellener@bc.edu>
2022-03-26 16:48:29 -0400

    6.17 Custom CollectionView Cells for OpenWeather Hourly Weather Forecast

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -21,6 +21,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var weatherDetail: WeatherDetail!
     var locationIndex = 0
@@ -31,6 +32,8 @@ class LocationDetailViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         updateUserInterface()
     }
@@ -64,6 +67,7 @@ class LocationDetailViewController: UIViewController {
                 self.summaryLabel.text = self.weatherDetail.summary
                 self.imageView.image = UIImage(named: self.weatherDetail.dayIcon)
                 self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         }
     }
@@ -111,3 +115,16 @@ extension LocationDetailViewController: UITableViewDelegate, UITableViewDataSour
         return 80
     }
 }
+
+extension LocationDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return weatherDetail.hourlyWeatherData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath) as! HourlyCollectionViewCell
+        hourlyCell.hourlyWeather = weatherDetail.hourlyWeatherData[indexPath.row]
+        return hourlyCell
+    }
+}
+
